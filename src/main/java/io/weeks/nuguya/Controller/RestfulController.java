4a1f3252b9cf72c37c9b5232693d2596ccce89ff
ByeonGooHoon <dksekfldks65@naver.com>
2020-02-13 01:35:58 +0900

    키워드 기반 Json 파싱 테스트

--- src/main/java/io/weeks/nuguya/Controller/RestfulController.java
+++ src/main/java/io/weeks/nuguya/Controller/RestfulController.java
@@ -1,9 +1,9 @@
 package io.weeks.nuguya.Controller;
 
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+
 
 @RestController
 public class RestfulController {
@@ -28,17 +25,40 @@ public class RestfulController {
     @RequestMapping("/restfulTest")
     public String home() {
 
-        String json = restTemplate.getForObject("http://localhost:5000/crawling/image/nuguya/cat/4", String.class);
+        String url = "http://localhost:5000/crawling/image/";
+        String appName = "nuguya";
+        String keywords = "아이린,수지,로제";
+        String num = "4";
+
+        String requestUrl = url + "/" + appName + "/" + keywords + "/" + num;
+
+        String json = restTemplate.getForObject(requestUrl, String.class);
         System.out.println(json);
         try {
 
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode jsonNode = mapper.readTree(json);
+            JSONParser jsonParser = new JSONParser();
 
-            String keyValue = jsonNode.get("cat").asText();
-            System.out.println("cat value : " + keyValue);
+            //JSON데이터를 넣어 JSON Object 로 만들어 준다.
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(json);
 
+            String[] keywordArray = keywords.split(",", 0);
+            int keywordLen = keywordArray.length;
 
+            for(int j=0;j<keywordLen;j++) {
+
+                //키워드별로 저장 위치 출력
+                JSONArray imageList = (JSONArray) jsonObject.get(keywordArray[j]);
+
+                for (int i = 0; i < imageList.size(); i++) {
+
+                    //배열 안에 있는것도 JSON형식 이기 때문에 JSON Object 로 추출
+                    Object fileInfoObj = (Object) imageList.get(i);
+
+                    //JSON name으로 추출
+                    System.out.println(fileInfoObj.toString());
+
+                }
+            }
 
         } catch(Exception e){
             System.out.println(e);
