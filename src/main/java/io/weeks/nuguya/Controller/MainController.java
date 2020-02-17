6c7b1951db97112489aa5d7937a035f35580a91b
ByeonGooHoon <dksekfldks65@naver.com>
2020-02-17 22:27:28 +0900

    CORS 테스트

--- src/main/java/io/weeks/nuguya/Controller/MainController.java
+++ src/main/java/io/weeks/nuguya/Controller/MainController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +26,12 @@ public class MainController {
 
     @Autowired
     private WritingTypeService writingTypeService;
+
+    @GetMapping("test")
+    public String getWritePage(Model model) {
+
+        return "test";
+    }
 
     /*
      ** 게임 유형 리스트 조회
