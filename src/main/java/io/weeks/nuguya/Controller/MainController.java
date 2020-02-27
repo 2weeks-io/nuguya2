8af741d8aace6f4d92eee99af56ad70faa13df31
ByeonGooHoon <dksekfldks65@naver.com>
2020-02-28 01:13:10 +0900

    젠킨스 연동 테스트

--- src/main/java/io/weeks/nuguya/Controller/MainController.java
+++ src/main/java/io/weeks/nuguya/Controller/MainController.java
@@ -30,7 +30,7 @@ public class MainController {
     @GetMapping("test")
     public String getWritePage(Model model) {
 
-        return "docker test";
+        return "docker test!!!!!!!!!!";
     }
 
     /*
