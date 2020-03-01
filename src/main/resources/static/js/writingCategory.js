c8affd0d9daad150472b6a8f410c64e7fd6a1323
ByeonGooHoon <dksekfldks65@naver.com>
2020-03-01 18:59:38 +0900

    게시글 상세 삭제 코드 이상해진거 수정

--- src/main/resources/static/js/writingCategory.js
+++ src/main/resources/static/js/writingCategory.js
@@ -1,34 +1,5 @@
 function bindObjectEvt() {
 
-    //게시글 상세 삭제
-    $("input:button[name=delBtn]").click(function () {
-        var writingNo  = $(this).data("writingno");
-        var writingSeq = $(this).data("writingseq");
-
-        var url = "/nuguya/writingDtl/" + writingNo + "/" + writingSeq;
-
-        param = {
-            writingNo     : writingNo
-            , writingSeq : writingSeq
-        };
-
-        $.ajax({
-            async       : true,
-            url         : url,
-            traditional : true,
-            type        : "DELETE",
-            data        : param,
-            cache       : false,
-            success     : function(result){
-                console.log(result.resultMsg);
-                $("#"+ result.writingDtl.writingSeq).hide();
-            },
-            error       : function(jqXhr, status, error){
-                alert("에러가 발생하였습니다.");
-            }
-        });
-
-    });
 }
 
 function redirectWritingDtlUpdate(obj){
