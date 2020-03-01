9d3ba0bec8a3e135f262727cd1a717007e228f06
ByeonGooHoon <dksekfldks65@naver.com>
2020-03-01 13:04:18 +0900

    카테고리 페이지 랜덤 리스트 반환 추가

--- src/main/java/io/weeks/nuguya/Repository/WritingRepository.java
+++ src/main/java/io/weeks/nuguya/Repository/WritingRepository.java
@@ -1,9 +1,12 @@
 package io.weeks.nuguya.Repository;
 
 import io.weeks.nuguya.Entity.Writing;
+import io.weeks.nuguya.Entity.WritingDtl;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -17,5 +20,8 @@ public interface WritingRepository extends JpaRepository<Writing, Long> {
 
     Page<Writing> findByWritingDivCd(Pageable pageable, String writingDivCd);
 
-    Page<Writing> findByWritingDivCdAndUseYn(Pageable pageable, String writingDivCd, String useYn);
+    //Page<Writing> findByWritingDivCdAndUseYn(Pageable pageable, String writingDivCd, String useYn);
+
+    @Query("select w from Writing w where w.writingDivCd = :writingDivCd and w.useYn = :useYn order by function('RAND')")
+    Page<Writing> findByWritingDivCdAndUseYn(Pageable pageable, @Param("writingDivCd") String writingDivCd, @Param("useYn") String useYn);
 }
